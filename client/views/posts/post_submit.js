1cd653653e7878190272b39365a79dc669a0331c
Sean Li <lishang106@gmail.com>
2014-01-27 16:45:46 -0500

    More Microscope.

---
+++ client/views/posts/post_submit.js
@@ -0,0 +1,15 @@
+Template.postSubmit.events({
+  'submit form': function(event) {
+    event.preventDefault();
+    var post = {
+      url: $(event.target).find('[name=url]').val(),
+      title: $(event.target).find('[name=title]').val(),
+      message: $(event.target).find('[name=message]').val()
+    }
+    Meteor.call('post', post, function(error, id) {
+      if (error)
+        return alert(error.reason);
+    });
+    Router.go('postsList');
+  }
+});
