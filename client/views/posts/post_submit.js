abfa51682c7d62c1aa8096a1c880068c9184761a
Sean Li <lishang106@gmail.com>
2014-01-27 22:18:46 -0500

    Error, comment and pagination.

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -1,15 +1,23 @@
 Template.postSubmit.events({
-  'submit form': function(event) {
-    event.preventDefault();
+  'submit form': function(e) {
+    e.preventDefault();
+
     var post = {
-      url: $(event.target).find('[name=url]').val(),
-      title: $(event.target).find('[name=title]').val(),
-      message: $(event.target).find('[name=message]').val()
+      url: $(e.target).find('[name=url]').val(),
+      title: $(e.target).find('[name=title]').val(),
+      message: $(e.target).find('[name=message]').val()
     }
+
     Meteor.call('post', post, function(error, id) {
-      if (error)
-        return alert(error.reason);
+      if (error) {
+        // display the error to the user
+        throwError(error.reason);
+
+        if (error.error === 302)
+          Router.go('postPage', {_id: error.details})
+      } else {
+        Router.go('postPage', {_id: id});
+      }
     });
-    Router.go('postsList');
   }
 });
